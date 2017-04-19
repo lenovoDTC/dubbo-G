f0263d33f36e7e4a2567950659e79c6527800033
Ning Ning1 Hao <haoning1@Lenovo.com>
2017-04-19 10:28:07 +0800

    dubbo 2.5.3 parameter convert to json string

--- dubbo-demo/dubbo-demo-consumer/src/test/java/com/alibaba/dubbo/demo/consumer/Person.java
+++ dubbo-demo/dubbo-demo-consumer/src/test/java/com/alibaba/dubbo/demo/consumer/Person.java
@@ -1,6 +1,7 @@
 package com.alibaba.dubbo.demo.consumer;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -39,13 +40,20 @@ public class Person {
     public static void main (String[] args) {
         //new Person();
 //        System.out.println(Person.a);
-        String sb = "{\"flag\":1,\"success\":1,\"expired\":0,\"page\":{\"data\":[{\"fxsno\":\"0110198385\",\"fxsname\":\"佳电（上海）管理有限公司（云贵）\",\"matnr\":\"90EACTO1WW0109829092\",\"mtart\":\"扬天T6900CI7-6700 8G 1T DVD-RW   2G独显\",\"quantity\":27,\"createby\":\"\",\"productgroupno\":\"68\",\"createdate\":\"2017-04-12 11:45:29\"}],\"pageSize\":10,\"pageindex\":1,\"sort\":1,\"sortfield\":\"fxsname\",\"total_number\":1}}";
-        JSONObject json = JSON.parseObject(sb);
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("status", 1);
-        jsonObject.put("body", sb);
-        System.out.println(JSON.toJSONString(jsonObject));
+//        String sb = "{\"flag\":1,\"success\":1,\"expired\":0,\"page\":{\"data\":[{\"fxsno\":\"0110198385\",\"fxsname\":\"佳电（上海）管理有限公司（云贵）\",\"matnr\":\"90EACTO1WW0109829092\",\"mtart\":\"扬天T6900CI7-6700 8G 1T DVD-RW   2G独显\",\"quantity\":27,\"createby\":\"\",\"productgroupno\":\"68\",\"createdate\":\"2017-04-12 11:45:29\"}],\"pageSize\":10,\"pageindex\":1,\"sort\":1,\"sortfield\":\"fxsname\",\"total_number\":1}}";
+//        JSONObject json = JSON.parseObject(sb);
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("status", 1);
+//        jsonObject.put("body", sb);
+//        System.out.println(JSON.toJSONString(jsonObject));
 
+        JSONObject json = new JSONObject();
+        JSONArray array = new JSONArray();
+        array.add("hahha");
+        array.add("hahha1");
+        json.put("a", array.toJSONString());
+
+        System.out.println(json.toJSONString());
 
     }
 
